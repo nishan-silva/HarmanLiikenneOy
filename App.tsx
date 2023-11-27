3e8a461a337e14df28792d4c37f727dfbf0dfe90
Nishan Silva <nishaan.silva@gmail.com>
2023-11-27 20:27:12 +0200

    adding testsuites folder

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: 'https://harmanliikenne.fi/seinajoen-paikallisliikenne/talviaikataulut/' }} // Replace with your desired URL
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
